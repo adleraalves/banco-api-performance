568eab96f975748bf7f495fb3f44ed4277b9b4bc
Adler <adleralves@gamil.com>
2026-03-31 20:53:35 -0300

    Adicionei VUs e Duration para a execução do teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,7 +2,9 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  iterations: 10,
+  //iterations: 10,
+  vus: 10,
+  duration: '30s',
   thresholds: {
     http_req_duration: ['p(90)<10', 'max<15'],
     http_req_failed: ['rate<0.01']
