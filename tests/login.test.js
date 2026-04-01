0b9e6214cb0863b488d61bdfdf308eca9d013f44
Adler <adleralves@gamil.com>
2026-03-31 21:20:01 -0300

    Adicionando Stages ao nosso teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -3,10 +3,19 @@ import { sleep, check } from 'k6';
 
 export const options = {
   //iterations: 10,
-  vus: 10,
-  duration: '30s',
+
+  //vus: 10,
+  //duration: '30s',
+
+  stages:[
+    { duration: '10s', target: 10},
+    { duration: '20s', target: 10},
+    { duration: '10s', target: 30},
+    { duration: '10s', target: 30},
+    { duration: '20s', target: 0}
+  ],
   thresholds: {
-    http_req_duration: ['p(90)<10', 'max<15'],
+    http_req_duration: ['p(90)<10', 'max<20'],
     http_req_failed: ['rate<0.01']
   }
 };
